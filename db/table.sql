72915c62abd8ee42e34c545394ab9791f4302723
John Pillar <jp@Johns-MacBook-Pro.local>
2020-09-18 21:06:06 +0100

    Migrate to Mongo and use new vault structure

--- db/table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE schedules (
-  id                 SERIAL      PRIMARY KEY,
-  hour               INT         NOT NULL,
-  minute             INT         NOT NULL,
-  active             BOOLEAN     NOT NULL DEFAULT TRUE
-)
